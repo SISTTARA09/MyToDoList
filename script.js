11336e71fe58da7062a51affec59959630f32a81
SISTTARA09 <yassinesisttara06@gmial.com>
2023-06-04 12:30:30 +0200

    add remove button

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let cl = console.log
 // GET ELEMENTS
 let form = document.getElementById('form');
 let input = document.getElementById('input');
-let submit = document.getElementById('submit');
+// let submit = document.getElementById('submit');
 let listParent = document.getElementById('listParent')
-let removeEle = document.createElement('span.remove-ele');
+// let removeEle = document.createElement('span.remove-ele');
 // FUNCTIONS
 let toDoArr = [];
 form.addEventListener('submit', ev => {
@@ -24,9 +24,22 @@ if (localStorage.getItem('key')) {
   toDoArr = keyItem;
   toDoArr.forEach(ele => {
     let list = document.createElement('p');
-    list.setAttribute('class', 'list')
+    list.setAttribute('class', 'list');
+    let removeEle = document.createElement('span');
+    removeEle.setAttribute('class', 'remove-ele');
+    removeEle.textContent = "remove";
     list.textContent = ele.title.toUpperCase();
+    list.appendChild(removeEle);
     listParent.append(list);
+    // WORK WITH REMOVE ITEM.
+    removeEle.addEventListener('click', ev => {
+      let target = ev.target;
+      let list = target.parentNode;
+      let listContent = list.textContent.replace('remove', '');
+      cl(listContent);
+      cl(toDoArr[0].id);
+      toDoArr[0] = '';
+    })
   });
   listParent.style = 'display: flex;';
   listParent.appendChild(button);
